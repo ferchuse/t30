6e6c4480b87aecacca5a165bb319925a3eaf5def
ferchuse <sistemas@glifo.mx>
2026-07-18 22:38:07 -0600

    Update plantilla_pdf.php

--- facturacion/facturacion/plantilla_pdf.php
+++ facturacion/facturacion/plantilla_pdf.php
@@ -197,7 +197,9 @@
 					<br>
 					<br>
 					<?php 
-						echo URL_SISTEMA."taquilla/paginas/facturacion/consultas/logos/".rawurlencode($factura["url_logo"]);
+						$url_logo = URL_SISTEMA."taquilla/paginas/facturacion/consultas/logos/".rawurlencode($factura["url_logo"]);
+						
+						echo "<img alt='Logo' src='$url_logo' class='img-responsive'>";
 					?>
 					
 				</div>  
